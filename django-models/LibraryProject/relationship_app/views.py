bf67324f3f75d475e1b89924724430082ff79321
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 16:50:23 +0300

    Bug Fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,5 +12,5 @@ def list_books(request):
 # CBV
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
