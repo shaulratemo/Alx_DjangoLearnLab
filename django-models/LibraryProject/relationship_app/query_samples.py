4af4779902307785414d414e88574d332835ffd8
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 16:09:25 +0300

    Add filter

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def books_by_author(author_name):
-    author = Author.objects.filter(name=author_name)
-    return author.books.all()
+    author = Author.objects.get(name=author_name)
+    return author.books.filter(author=author)
 
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
