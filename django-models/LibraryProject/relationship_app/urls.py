5168ff49970249018ca54943789473b0cf60bfbc
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 17:34:23 +0300

    Bug Fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import RegisterView, list_books, LibraryDetailView
-from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    path("login/", LoginView.as_view(template_name="relationship_app/login.html", name="login")),
-    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html", name="logout")),
-    path("register/", RegisterView.as_view(), name="register"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
-from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 # FBV
@@ -21,7 +18,31 @@ class LibraryDetailView(DetailView):
     context_object_name = "library"
 
 # Registration
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    template_name = "relationship_app/register.html"
-    success_url = reverse_lazy("login")
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log the user in after registration
+            return redirect("list_books")  # redirect to books page (or home)
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+# User Login
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("list_books")  # redirect after login
+    else:
+        form = AuthenticationForm()
+    return render(request, "relationship_app/login.html", {"form": form})
+
+
+# User Logout
+def logout_view(request):
+    logout(request)
+    return render(request, "relationship_app/logout.html")
