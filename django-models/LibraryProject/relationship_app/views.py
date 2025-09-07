6da491c19e315352bf7c0e19f00b61d42608d667
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 16:48:55 +0300

    Bug fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 from django.views.generic import DetailView
 
 # Create your views here.
@@ -11,5 +12,5 @@ def list_books(request):
 # CBV
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"
+    template_name = "library_detail.html"
     context_object_name = "library"
