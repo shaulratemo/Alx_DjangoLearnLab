70df91fc9263de26d5ce150fc8fb380d1972cde1
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 16:44:50 +0300

    Add Views - FBV & CBV

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
 
 # Create your views here.
+# FBV
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+# CBV
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
