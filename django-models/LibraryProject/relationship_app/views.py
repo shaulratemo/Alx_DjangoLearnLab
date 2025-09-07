ce8bae8e237d7c090ebd3c7f472be2fedb09c88b
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-07 18:04:38 +0300

    Bug fix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 # FBV
