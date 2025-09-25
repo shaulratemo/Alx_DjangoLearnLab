b2ffea659ed7003ddd2199f8d6b6082c4a313dac
Shaul Ratemo <ratemoshaul@gmail.com>
2025-09-25 16:15:25 +0300

    Update urls

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
     path('books/create/', BookCreateView.as_view(), name='book_create'),
-    path('books/update/', BookUpdateView.as_view(), name='book_update'),
-    path('books/delete/', BookDeleteView.as_view(), name='book_delete'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),
 ]
