3aa263ddf8e76ff9337f40f30a4eff41fdcec92d
Shaul Ratemo <ratemoshaul@gmail.com>
2025-10-01 09:52:06 +0300

    Change settings to use PostgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog_db',   # database name
+        'USER': 'postgres',         # your postgres username
+        'PASSWORD': 'supreme2019', # replace with your password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -117,10 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static"),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
