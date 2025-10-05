8092532352817168a185717a1c9b34ebb2446d73
Shaul Ratemo <ratemoshaul@gmail.com>
2025-10-05 16:03:11 +0300

    Removed nested git folders

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# Register
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+        
+        else:
+            form = CustomUserCreationForm()
+        return render(request, "blog/register.html", {"form": form})
+    
+# Login
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = AuthenticationForm()
+    return render(request, "blog/login.html", {"form": form})
+
+# Logout
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+# Profile
+@login_required
+def profile_view(request):
+    return render(request, "blog/profile.html", {"user": request.user})
